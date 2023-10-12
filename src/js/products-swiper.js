be362e38f5e841c5744b6e6f9c0c0dcb7662f76b
Anastasia Verbenets <124292516+AnastasiaVerbenets@users.noreply.github.com>
2023-10-12 20:11:22 +0300

    update

---
+++ src/js/products-swiper.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    breakpoints: {
+        375: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        768: {
+          slidesPerView: 2.5,
+          spaceBetween: 18,
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 18,
+        },
+      },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
