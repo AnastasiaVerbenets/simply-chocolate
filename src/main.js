e4229c7f17bbed8a9043f976aff1cd2ce40ce949
Anastasia Verbenets <124292516+AnastasiaVerbenets@users.noreply.github.com>
2023-10-31 00:15:22 +0200

    up

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-// import "./js/mobile-menu";
+import "./js/mobile-menu";
 import "./js/products-swiper";
 import "./js/opinions-swiper";
-// import "./js/modal";
+import "./js/modal";
 import "./js/video";
 import "./js/buy-now-swiper";
-
-import "./js/read-more";
+import "./js/scroll";
+import "./js/scroll-to-top";
+import "./js/opinions-modal";
