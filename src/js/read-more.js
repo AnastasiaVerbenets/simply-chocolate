223d53b3bc8f415bb4692a7d930783e0f7f67bbc
Anastasia Verbenets <124292516+AnastasiaVerbenets@users.noreply.github.com>
2023-10-31 17:58:38 +0200

    changes

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,12 +1,12 @@
 function readMore() {
-    var dots = document.getElementsByClassName("dots");
-    var more = document.getElementsByClassName("more");
+    var dots = document.getElementsByClassName('.dots');
+    var more = document.getElementsByClassName('.more');
 
-    if(dots.style.display === "none") {
-        dots.style.display="inline";
-        more.style.display="none";
+    if(dots.style.display === 'none') {
+        dots.style.display='inline';
+        more.style.display='none';
     } else {
-        dots.style.display="none";
-        more.style.display="inline";
+        dots.style.display='none';
+        more.style.display='inline';
     }
 }
