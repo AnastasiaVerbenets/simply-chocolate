606f03b2085a20f70806f34a8609013ef702a4c0
pla1n <danilkuisis@gmail.com>
2023-11-02 18:26:11 +0200

    upup

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import "./js/mobile-menu";
+// import "./js/mobile-menu";
 import "./js/products-swiper";
 import "./js/opinions-swiper";
-import "./js/modal";
+// import "./js/modal";
 import "./js/video";
 import "./js/buy-now-swiper";
-import "./js/scroll";
-import "./js/scroll-to-top";
+
 import "./js/read-more";
