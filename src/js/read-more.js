b4eb3e4338a759608f71b310a7e40800816c43a2
pla1n <danilkuisis@gmail.com>
2023-11-06 20:58:16 +0200

    update

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,12 +1,35 @@
 function readMore() {
-    var dots = document.getElementsByClassName('.dots');
-    var more = document.getElementsByClassName('.more');
+    
+    var containers = document.querySelectorAll('.ingredients__hover-container');
 
-    if(dots.style.display === 'none') {
-        dots.style.display='inline';
-        more.style.display='none';
-    } else {
-        dots.style.display='none';
-        more.style.display='inline';
-    }
+    console.log("hello");
+
+    containers.forEach((container) => {
+        var btn = container.querySelector('.morebtn');
+        var hbtns = container.querySelector('.ingredients__hover-btns');
+        var htitle = container.querySelector('.ingredients__hover-title');
+        var dots = btn.querySelector('.dots');
+        var more = btn.querySelector('.more');
+
+        btn.addEventListener('click' , () => {
+            if(dots.style.display === 'none') {
+                dots.style.display='inline';
+                more.style.display='none';
+                hbtns.style.display='block';
+                htitle.style.display='block';
+            } else {
+                dots.style.display='none';
+                more.style.display='inline';
+                hbtns.style.display='none';
+                htitle.style.display='none';
+            }
+    
+    
+        }
+        )
+    })
+
+    
 }
+
+document.addEventListener("DOMContentLoaded", readMore);
