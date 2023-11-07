997ef6fded2947f3ad788a7fe8f652ed013ef50c
Anastasia Verbenets <124292516+AnastasiaVerbenets@users.noreply.github.com>
2023-11-07 19:41:14 +0200

    modals

--- src/js/modal.js
+++ src/js/modal.js
@@ -34,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
   modalButtons.forEach(function (item) {
     item.addEventListener('click', function (e) {
       e.preventDefault();
+      document.body.classList.add('modal-open');
 
       var modalId = this.getAttribute('data-modal'),
         modalElem = document.querySelector(
@@ -49,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
   closeButtons.forEach(function (item) {
     item.addEventListener('click', function (e) {
       var parentModal = this.closest('.modal');
+      document.body.classList.remove('modal-open');
 
       parentModal.classList.remove('active');
       overlay.classList.remove('active');
@@ -62,6 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
       var key = e.keyCode;
 
       if (key == 27) {
+        document.body.classList.remove('modal-open');
         document.querySelector('.modal.active').classList.remove('active');
         document.querySelector('.overlay').classList.remove('active');
       }
@@ -69,9 +72,48 @@ document.addEventListener('DOMContentLoaded', function () {
     false
   );
 
-  overlay.addEventListener('click', function () {
+  overlay.addEventListener('click', function (e) {
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+
     document.querySelector('.modal.active').classList.remove('active');
     this.classList.remove('active');
+    document.body.classList.remove('modal-open');
     document.body.style.overflow = '';
+  });
+
+   $(document).ready(function () {
+    $(function () {
+      $('#js-buy-now-modal').submit(function (e) {
+        e.preventDefault();
+        $('#js-thanks-buy-now-modal')
+          .attr('data-modal', '4')
+          .addClass('js-open-modal');
+
+        setTimeout(() => {
+          document.querySelector('.modal.active').classList.remove('active');
+          this.classList.remove('active');
+          document.body.classList.remove('modal-open');
+          $('.pop-up_buy-now').addClass('active');
+        }, 300);
+      });
+    });
+
+    $(function () {
+      $('#js-opinions-modal').submit(function (e) {
+        e.preventDefault();
+        $('#js-review-modal')
+          .attr('data-modal', '5')
+          .addClass('js-open-modal');
+
+        setTimeout(() => {
+          document.querySelector('.modal.active').classList.remove('active');
+          this.classList.remove('active');
+          document.body.classList.remove('modal-open');
+          $('.pop-up_opinions').addClass('active');
+        }, 300);
+      });
+    });
   });
 }); 
